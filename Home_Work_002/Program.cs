1341a93dc190add6f23b03516616bdce4c90d2be
Almat32 <raha-rambler.ru>
2022-09-02 21:37:57 +0600

    выполнено 2-e задание

---
+++ Home_Work_002/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое значение ");
+string numa = Console.ReadLine();
+int a = Convert.ToInt32(numa);
+
+Console.WriteLine("Введите второе значение ");
+string numb = Console.ReadLine();
+int b = Convert.ToInt32(numb);
+
+Console.WriteLine("Введите третье значение ");
+string numc = Console.ReadLine();
+int c = Convert.ToInt32(numc);
+
+int max = a;
+
+if(b > max) max = b;
+if(c > max) max = c;
+
+Console.WriteLine("Максимум " + (max));
