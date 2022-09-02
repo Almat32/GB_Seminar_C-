db748ffced27518d68a02bf26b72b3791c34d957
Almat32 <raha-rambler.ru>
2022-09-02 18:43:52 +0600

    Начало работы

--- Seminar_001/Program.cs
+++ Seminar_001/Program.cs
@@ -1,1 +1,11 @@
-﻿//
+﻿/* // типы данных, название и данные
+int = 10; // int - целочисленные данные
+float b = 12.4f; // - float дробный числа
+double c = 8.4; // double дробный числа
+string d = "Hello World"; //  string - строковые данные
+bool e = true // false // bool данные с условием тое есть истина или ложь */
+
+
+Console.Writeline("Введите имя ");
+string name = Console.ReadLine();
+Console.Writeline("Hello " + name + 2);
