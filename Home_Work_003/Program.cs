00c596d8fcb06e50a9bffa9d283f98945dac008d
Almat32 <raha-rambler.ru>
2022-09-03 20:37:33 +0600

    add Home Work 3

---
+++ Home_Work_003/Program.cs
@@ -0,0 +1,7 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+if(number * 2 == 0)
+{
+    Console.WriteLine("да");
+}
+Console.WriteLine("нет");
---
+++ Seminar_002/Program.cs
@@ -0,0 +1,12 @@
+﻿/* int[] number = new int[4];
+int[] num = {1,2,3,4,5,6,7,8,9};
+Console.WriteLine
+ */
+ 
+Console.WriteLine("введите число ");
+int num_user = int.Parse(Console.ReadLine());
+int[] number = new int[num_user];
+for (int = 0; int < number.Length; int++)
+{
+    number[i] = Random().
+}
