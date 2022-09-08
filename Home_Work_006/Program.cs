bcab28029fce0dd9f963b65e3f93de40a080bc18
Astronautix <raha-32@rambler.ru>
2022-09-08 21:02:20 +0600

    ДЗ 2, задание 2,4

---
+++ Home_Work_006/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите трез занчное число");
+string number = Console.ReadLine();
+if(number.Length > 2)
+{
+    string StrNum = number.Substring(2,1);
+    int num = int.Parse(StrNum);
+    Console.WriteLine("Значение третьего индекса: " + number[2]);
+    
+}
+
+else
+{
+    
+    Console.WriteLine("третьей строки нету");
+}
---
+++ Home_Work_007/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите день недели ");
+int day = int.Parse(Console.ReadLine());
+if(day < 8 && day > 0)
+{
+    if(day == 7)
+    {
+        Console.WriteLine("Выходной");
+    }
+    else
+    {
+        Console.WriteLine("Рабочий день");
+    }
+}
+else
+{
+    Console.WriteLine("Введенное число больше дней в неделе");
+}
